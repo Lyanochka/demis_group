df02398459cd9ca9fcdc64ceda1402d56c3d80dd
Lyanochka <lyanochka_@mail.ru>
2016-12-04 19:10:47 +0300

    Method statToString was added.

--- src/DemisGroupTask.java
+++ src/DemisGroupTask.java
@@ -11,8 +11,8 @@ public class DemisGroupTask {
     /**
      * Метод поиска всех блоков в заданном тексте с введеными разделителями
      * @param text - исходный текст
-     * @param leftBorder - правый разделитель
-     * @param rightBorder - левый разделитель
+     * @param leftBorder - левый разделитель
+     * @param rightBorder - правый разделитель
      * @return - динамический массив из найденых блоков
      */
     ArrayList<String> findBlocks (String text, char leftBorder, char rightBorder) {
@@ -24,7 +24,7 @@ public class DemisGroupTask {
             if (text.charAt(i)==leftBorder) {
                 // запоминаем индекс следующей позиции
                 int startPos=i+1;
-                // создаем флаг для выхода из цикла поиска прового разделителя
+                // создаем флаг для выхода из цикла поиска правого разделителя
                 boolean isBorderFound=false;
                 // поиск правого разделителя
                 for (i=i+1;i<text.length()&& !isBorderFound;i++){
@@ -33,14 +33,24 @@ public class DemisGroupTask {
                         // меняем значение флага выхода из цикла
                         isBorderFound=true;
                         // если между разделителями есть хотя бы один символ
-                        if (i-startPos>1) {
+                        if (i-startPos>=1) {
                             // добавляем найденную подстроку к результату
                             blocks.add(text.substring(startPos, i));
                         }
                     }
                 }
-                // сдвикаем итератор на позицию влево
-                i=i-1;
+                // если разделители не совпадают
+                if (leftBorder!=rightBorder) {
+                    // сдвикаем итератор на позицию влево
+                    i=i-1;
+                }
+                // иначе
+                else{
+                    // сдвикаем итератор на две позиции влево, если мы не дошли до конца текста
+                    if (i!=text.length()) {
+                        i = i - 2;
+                    }
+                }
             }
         }
         return blocks;
@@ -49,16 +59,16 @@ public class DemisGroupTask {
     /**
      * Метод, предоставляющий отчет по найденным блокам текста в формате JSON
      * @param text - исходный текст
-     * @param leftBorder - правый разделитель
-     * @param rightBorder - левый разделитель
-     * @return - отчет по найденным блокав в формате JSON
+     * @param leftBorder - левый разделитель
+     * @param rightBorder - правый разделитель
+     * @return - отчет по найденным блокам в формате JSON
      */
     JSONObject getStat(String text, char leftBorder, char rightBorder){
         JSONObject stat=new JSONObject();
         // находим все подстроки для заданных разделителей
         ArrayList<String> blocks = findBlocks(text,leftBorder,rightBorder);
-        // добавлям в отчет информацию о количестве подстрок
-        stat.put("TotalBlocks",blocks.size());
+        // добавлям в отчет информацию о количестве найденных подстрок
+        stat.put("FoundedBlocksCount",blocks.size());
         // находим среднюю длину блока текста в символах и добавлям ее в отчет
         double totalLength=0;
         for (int i=0;i<blocks.size();i++){
@@ -111,14 +121,14 @@ public class DemisGroupTask {
             curBlockStat.put("TotalOther",other);
             blockStatAr.put(curBlockStat);
         }
-        stat.put("BlocksStat",blockStatAr);
+        stat.put("StatOfBlocks",blockStatAr);
         return stat;
     }
 
     /**
-     * Проверка на принадлежность кирилице
+     * Проверка на принадлежность кириллице
      * @param c - символ в тексте
-     * @return - истина, если символ кирилицы
+     * @return - истина, если символ кириллицы
      */
     boolean isRussianSymbol(char c){
         char smallLeft='а',smallRight='я';
@@ -127,7 +137,7 @@ public class DemisGroupTask {
     }
 
     /**
-     * Проверка на принадлежность латиницы
+     * Проверка на принадлежность латинице
      * @param c - символ в тексте
      * @return - истина, если символ латиницы
      */
@@ -146,17 +156,77 @@ public class DemisGroupTask {
         return Character.isDigit(c);
     }
 
+    /**
+     * Метод для преобразования отчета формата JSON в строку, соответствующую требованиям задания
+     * @param stat - полученная статистика о тексте в формате JSON
+     * @return - строка с результатом преобразования
+     */
 
-    public static void main(String[] args){
-        Scanner scan=new Scanner(System.in);
+    String statToString(JSONObject stat){
+        String res="Full text stat:\n";
+
+        String curKey="FoundedBlocksCount";
+        Integer totalBlocks=(Integer)stat.get(curKey);
+        res+=curKey+": "+String.valueOf(totalBlocks)+"\n";
+
+        curKey="AverageBlockLength";
+        Double averLen=(Double)stat.get(curKey);
+        res+=curKey+": "+String.valueOf(averLen)+"\n";
+
+        curKey="MaxBlockLength";
+        Integer maxBLen=(Integer)stat.get(curKey);
+        res+=curKey+": "+String.valueOf(maxBLen)+"\n";
+
+        curKey="MinBlockLength";
+        Integer minBLen=(Integer)stat.get(curKey);
+        res+=curKey+": "+String.valueOf(minBLen)+"\n";
+
+        JSONArray blocksAr=(JSONArray)stat.get("StatOfBlocks");
+        if (blocksAr.length()>0){
+            res+="\nStat for every block:\n\n";
+            for (int i = 0; i < blocksAr.length(); i++) {
+                JSONObject curObjStat=blocksAr.getJSONObject(i);
+
+                curKey="BlockValue";
+                String blockV=(String)curObjStat.get(curKey);
+                res+=curKey+": "+blockV+"\n";
+
+                curKey="BlockLength";
+                Integer curBlockLen=(Integer)curObjStat.get(curKey);
+                res+=curKey+": "+String.valueOf(curBlockLen)+"\n";
+
+                curKey="TotalLat";
+                Integer latCount=(Integer)curObjStat.get(curKey);
+                res+=curKey+": "+String.valueOf(latCount)+"\n";
+
+                curKey="TotalCyr";
+                Integer cyrCount=(Integer)curObjStat.get(curKey);
+                res+=curKey+": "+String.valueOf(cyrCount)+"\n";
+
+                curKey="TotalDigit";
+                Integer digCount=(Integer)curObjStat.get(curKey);
+                res+=curKey+": "+String.valueOf(digCount)+"\n";
+
+                curKey="TotalOther";
+                Integer otherCount=(Integer)curObjStat.get(curKey);
+                res+=curKey+": "+String.valueOf(otherCount)+"\n";
+
+                res+="\n";
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // введенные данные должны быть разделены переводом строк
         scan.useDelimiter("\\n");
-        String text =scan.next();
+        String text = scan.next();
 
-        char leftBorder=scan.next().charAt(0);
-        char rightBorder=scan.next().charAt(0);
-        DemisGroupTask finder=new DemisGroupTask();
-        JSONObject stat=finder.getStat(text,leftBorder,rightBorder);
-        System.out.println(stat.toString());
+        char leftBorder = scan.next().charAt(0);
+        char rightBorder = scan.next().charAt(0);
+        DemisGroupTask finder = new DemisGroupTask();
+        JSONObject stat = finder.getStat(text, leftBorder, rightBorder);
+        System.out.println(finder.statToString(stat));
     }
 }
